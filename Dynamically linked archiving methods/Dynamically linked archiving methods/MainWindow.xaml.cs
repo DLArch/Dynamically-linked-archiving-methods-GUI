e8f17b29f05d65ebf7395244303491e4c2b1bbad
Oleg <chukoleg@gmail.com>
2016-09-18 13:55:42 +0300

    pa1
    Partically worked file browser

--- Dynamically linked archiving methods/Dynamically linked archiving methods/MainWindow.xaml.cs
+++ Dynamically linked archiving methods/Dynamically linked archiving methods/MainWindow.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Shapes;
 using System.Linq;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
+//using shell32.dll;
 
 namespace Dynamically_linked_archiving_methods
 {
@@ -29,19 +30,34 @@ namespace Dynamically_linked_archiving_methods
             this.textBox.Text = System.IO.Directory.GetCurrentDirectory();
         }
 
+        /// <summary>
+        /// Установка дирректории из textbox
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        /// TODO Отловить исключениядоступа к файлам
         private void button1_Click(object sender, RoutedEventArgs e)
         {
+            //System.Windows.Forms.MessageBox.Show(System.IO.Directory.Exists(this.textBox.Text) ? "True" : "False");
+            if (this.textBox.Text[this.textBox.Text.Count()-1] == System.IO.Path.VolumeSeparatorChar)
+                this.textBox.Text += System.IO.Path.DirectorySeparatorChar;
             if (System.IO.Directory.Exists(this.textBox.Text))
                 System.IO.Directory.SetCurrentDirectory(this.textBox.Text);
             else
-                System.Windows.Forms.MessageBox.Show("Путь несуществует. Укажите другой путь");
+                System.Windows.Forms.MessageBox.Show("Путь не существует. Укажите другой путь");
             this.treeView.Items.Clear();
             foreach (string x in System.IO.Directory.GetFileSystemEntries(System.IO.Directory.GetCurrentDirectory()))
             {
                 this.treeView.Items.Add(x);
             }
+            //this.treeView.SetValue(new DependencyPropertyKey(), this.textBox.Text.Where(x => x == System.IO.Path.DirectorySeparatorChar).Count());
         }
 
+        /// <summary>
+        /// В treeView запишится путь к файлу, если перетянут файл
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void treeView_Drop(object sender, DragEventArgs e)
         {
             if (e.Data.GetDataPresent(System.Windows.DataFormats.FileDrop))
@@ -53,7 +69,7 @@ namespace Dynamically_linked_archiving_methods
                 }
             }
         }
-
+        
         private void treeView_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.Key == System.Windows.Input.Key.Delete)
@@ -61,6 +77,15 @@ namespace Dynamically_linked_archiving_methods
                 //int SItem = this.treeView.Items.IndexOf(this.treeView.SelectedItem);
                 this.treeView.Items.Remove(this.treeView.SelectedItem);
             }
+            else
+                if (e.Key == System.Windows.Input.Key.Back)
+                {
+                    this.textBox.Text = string.Concat(this.textBox.Text.Take(this.textBox.Text.Count() - (this.textBox.Text.Reverse().TakeWhile(x => x != System.IO.Path.DirectorySeparatorChar).Count() + 1)));
+                    button1_Click(sender, new System.Windows.RoutedEventArgs());
+                    //bool if_ = false;
+                    //System.IO.Directory.GetLogicalDrives.Where(x => x ==)
+                    //this.textBox.Text = System.IO.Directory.GetParent(this.textBox.Text).FullName;
+                }
         }
 
         private void button2_Click(object sender, RoutedEventArgs e)
@@ -68,10 +93,22 @@ namespace Dynamically_linked_archiving_methods
             this.treeView.Items.Clear();
         }
 
+        /// <summary>
+        /// Если нажата "Enter", установить путь из textBox.Text
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void textBox_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.Key == System.Windows.Input.Key.Enter)
-                this.button1_Click(sender, new RoutedEventArgs());
+                this.button1_Click(sender, new System.Windows.RoutedEventArgs());
+        }
+
+        private void treeView_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == System.Windows.Input.MouseButtonState.Pressed)
+                this.textBox.Text = (String)this.treeView.SelectedItem;
+            this.button1_Click(sender, new System.Windows.RoutedEventArgs());
         }
     }
 }
